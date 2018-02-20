6fd0f2644b2beec8aaa0e2b5dc2d089bd4cb53f0
ajzett <36409591+ajzett@users.noreply.github.com>
2018-02-20 12:56:56 -0500

    Add more functions

--- Gilbo-API.py
+++ Gilbo-API.py
@@ -1,5 +1,16 @@
 from abc import ABC, abstractmethod
+from random import *
 
-class entity(ABC)
-  def __init__(self, name):
+def rndNum(high, low = 1):
+    return randint(low, high)
+
+def type(phrase):
     pass
+
+# Abstract class from which all enemies, bosses, vendors, NPCs, and players are derived.
+class entity(ABC):
+    def __init__(self, name):
+        self.name = name
+
+    def say(phrase):
+        pass
