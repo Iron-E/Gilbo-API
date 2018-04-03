a2f4acee943f86043894952f14432cc08545d807
ajzett <36409591+ajzett@users.noreply.github.com>
2018-04-02 22:43:25 -0400

    Update Testing.py

--- Testing.py
+++ Testing.py
@@ -1,5 +1,21 @@
-import Gilbo
+import Gilbo as G
 
-test_map = matrix_map('eat ass')
-test_map.layout = np.array([[Tiles.Grass.value, Tiles.Grass.value, Tiles.Grass.value, Tiles.Grass.value], [Tiles.Grass.value, Tiles.Building.value, Tiles.Mountain.value, Tiles.Mountain.value]])
-loc_man.load_map(test_map, 4)
+test_map = G.matrix_map('eat ass')
+test_map.layout = G.np.array([[G.Tiles.Grass.value, G.Tiles.Grass.value, G.Tiles.Grass.value, G.Tiles.Grass.value], [G.Tiles.Grass.value, G.Tiles.Building.value, G.Tiles.Mountain.value, G.Tiles.Mountain.value]])
+G.loc_man.load_map(test_map, 4)
+
+smash = G.attack(100, 'You use your entire body to smash the opponent.')
+doodle = G.weapon('Wackadoodle', 'A mysterious doodle of some kind. Wacky.', 5, 100, 5, [smash])
+jim_collection = G.player_collection([doodle], [doodle])
+jim_stats = G.player_stats(50, 100, 10, 30, 100)
+jim = G.player('Jimbo', test_map, 1, 2, jim_collection, 20, jim_stats)
+
+new_item = jim.collection.inventory[0].__class__.__mro__[2]('you', 'suck', 20, 20)
+print(jim.collection.inventory[0].__class__.__mro__)
+if G.item_weighted in jim.collection.inventory[0].__class__.__mro__:
+    print('true\n')
+else:
+    print('false\n')
+
+G.tracker.update_tracker()
+# print(G.tracker.tracker['Gilbo.entity'])
