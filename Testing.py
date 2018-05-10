6309f73fe19936fb485edd65ef35b35afde034de
ajzett <adam.joseph.zett@gmail.com>
2018-05-10 17:15:25 -0400

    Update Testing.py for bat_man tests

--- Testing.py
+++ Testing.py
@@ -16,23 +16,27 @@ test2 = test_matrix_map('tortelini')
 test1.layout = G.np.array([[G.Tiles.Grass, G.Tiles.Grass, G.Tiles.Grass, G.Tiles.Grass], [G.Tiles.Grass, G.Tiles.Building, G.Tiles.Mountain, G.Tiles.Mountain]])
 test2.layout = G.np.array([[G.Tiles.Cave, G.Tiles.Water, G.Tiles.Building, G.Tiles.Building], [G.Tiles.Dirt, G.Tiles.Ice, G.Tiles.Wall, G.Tiles.Lava]])
 
-# Stuff for Jimbo
-smash = G.attack('Basic Smash', 15, 'You use your entire body to smash the opponent.')
-doodle = G.weapon('Wackadoodle', 'A mysterious doodle of some kind. Wacky.', 5, 100, [smash], 5)
-diddle = G.weapon('Wackadoodle', 'A mysterious doodle of some kind. Wacky.', 5, 100, [smash], 5)
+# Items
+test_debuff = G.buff_item('Test Debuff', 'Debuff to Test use_debuff()', 0, 5, 0, 0, -10)
+use_test_debuff = G.item('debuff_player() Ammo', 'Item to use debuff_player()', 0)
+
+# Attacks
+smash = G.attack('Basic Smash', 'You use your entire body to smash the opponent.', 15)
+debuff_player = G.ammo_attack('Debuff Test', "Debuff target's armor.", 8, use_test_debuff, 1, 100, 1, test_debuff)
+
+# Weapons
+doodle = G.weapon('Wackadoodle', 'A mysterious doodle of some kind. Wacky.', 5, 15, [smash], 5)
+diddle = G.weapon('Wacky Test Diddle', 'A dubious test item.', 10, 40, [smash, debuff_player])
+
+# Define Player
+jim_stats = G.battler_stats(50, 30, 45, 20, 1)
 jim_collection = G.player_collection(20, [doodle], [doodle])
-test_enemy_collection = G.battler_collection(20, [doodle], [doodle])
-jim_stats = G.battler_stats(50, 100, 20, 30, 10)
-test_enemy_stats = G.battler_stats(50, 45, 10, 50, 100)
 jim = G.player('Jimbo', test1, 2, 1, jim_collection, jim_stats)
-test_enemy = G.player('Enemy', test1, 2, 1, test_enemy_collection, test_enemy_stats)
 
-G.loc_man.load_map(test1)
+# Define Test Enemy
+test_enemy_stats = G.battler_stats(50, 45, 10, 50, 100)
+test_enemy_collection = G.battler_collection(20, [diddle, use_test_debuff, use_test_debuff], [diddle])
+test_enemy = G.battler('Enemy', test1, 2, 1, test_enemy_collection, test_enemy_stats)
 
-G.tracker.update_tracker(globals())
-
-G.loc_man.move(jim, G.Directions.Up)
-G.loc_man.teleport(jim, test2, 2, 0)
-G.loc_man.move(jim, G.Directions.Left)
-
+# Battle jim and test_enemy
 G.bat_man.battle(jim, test_enemy)
