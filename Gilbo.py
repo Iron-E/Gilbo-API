b7482e81f61ca50d0e3f295f5b36119855058bdf
ajzett <36409591+ajzett@users.noreply.github.com>
2018-05-15 08:01:37 -0400

    Update Gilbo.py

--- Gilbo.py
+++ Gilbo.py
@@ -1,4 +1,4 @@
-# Gilbo RPG API -- Version 1.0.0-beta7 #
+# Gilbo RPG API -- Version 1.0.0-beta8 #
 
 from abc import ABC, abstractmethod
 from enum import IntEnum, auto
@@ -809,13 +809,13 @@ class battle_manager(ABC):
         self.e = 2.7182
         self.battle_dict = {'turn': 0, 'total_turns': 0}
 
-        self.battle_dict['effect_dict'] = {'active_effect_enemy': False}
-        self.battle_dict['effect_dict']['active_effect_player'] = False
-
-        self.battle_dict['effect_dict']['reverse_effect_enemy'] = []
-        self.battle_dict['effect_dict']['reverse_effect_player'] = []
+        self.battle_dict['effect_dict'] = {'reverse_effect_player': [], 'reverse_effect_enemy': []}
 
         self.battle_dict['ai'] = {'used_item': 0}
+
+    @property
+    def effect_dict(self):
+        return self.battle_dict['effect_dict']
 
     def randnum(self, hi, lo=1):
         from random import randint
@@ -837,37 +837,32 @@ class battle_manager(ABC):
 
     def clean_active_effect(self):
         i = 0
-        while i < len(self.battle_dict['effect_dict']['reverse_effect_player']):
-            if (self.battle_dict['effect_dict']['reverse_effect_player'] != []) and (self.battle_dict['effect_dict']['reverse_effect_player'][i][0] < self.battle_dict['total_turns']):
-                del self.battle_dict['effect_dict']['reverse_effect_player'][i]
+        while i < len(self.effect_dict['reverse_effect_player']):
+            if (self.effect_dict['reverse_effect_player'] != []) and (self.effect_dict['reverse_effect_player'][i][0] < self.battle_dict['total_turns']):
+                del self.effect_dict['reverse_effect_player'][i]
             else:
                 i += 1
 
         i = 0
-        while i < len(self.battle_dict['effect_dict']['reverse_effect_enemy']):
-            if (self.battle_dict['effect_dict']['reverse_effect_enemy'] != []) and (self.battle_dict['effect_dict']['reverse_effect_enemy'][i][0] < self.battle_dict['total_turns']):
-                del self.battle_dict['effect_dict']['reverse_effect_enemy'][i]
+        while i < len(self.effect_dict['reverse_effect_enemy']):
+            if (self.effect_dict['reverse_effect_enemy'] != []) and (self.effect_dict['reverse_effect_enemy'][i][0] < self.battle_dict['total_turns']):
+                del self.effect_dict['reverse_effect_enemy'][i]
             else:
                 i += 1
 
         del i
 
     def refresh_active_effect(self, plyr, enemy):
-        if (self.battle_dict['effect_dict']['active_effect_player'] is True) or (self.battle_dict['effect_dict']['active_effect_enemy'] is True):
-            if self.battle_dict['effect_dict']['active_effect_player'] is True:
-                for i in self.battle_dict['effect_dict']['reverse_effect_player']:
+        if (self.effect_dict['reverse_effect_player'] != []) or (self.effect_dict['reverse_effect_enemy'] != []):
+            if self.effect_dict['reverse_effect_player'] != []:
+                for i in self.effect_dict['reverse_effect_player']:
                     if self.battle_dict['turn'] == i[0]:
                         self.use_item_stat(plyr, i[1])
 
-                if self.battle_dict['effect_dict']['reverse_effect_player'] == []:
-                    self.battle_dict['effect_dict']['active_effect_player'] = False
-
-            if self.battle_dict['effect_dict']['active_effect_enemy'] is True:
-                for i in self.battle_dict['effect_dict']['reverse_effect_enemy']:
+            if self.effect_dict['reverse_effect_enemy'] != []:
+                for i in self.effect_dict['reverse_effect_enemy']:
                     if self.battle_dict['turn'] == i[0]:
                         self.use_item_stat(plyr, i[1])
-                if self.battle_dict['effect_dict']['reverse_effect_enemy'] == []:
-                    self.battle_dict['effect_dict']['active_effect_enemy'] = False
 
             self.clean_active_effect()
 
@@ -881,13 +876,17 @@ class battle_manager(ABC):
         try:
             if itm.duration > 0:
                 if isinstance(thing, player):
-                    self.battle_dict['effect_dict']['active_effect_player'] = True
-                    self.battle_dict['effect_dict']['reverse_effect_player'].append((self.battle_dict['turn'] + itm.duration, self.reverse_item_stat(itm.stat_changes)))
-                    self.battle_dict['effect_dict']['reverse_effect_player'].sort()
+                    to_append = (self.battle_dict['turn'] + itm.duration, self.reverse_item_stat(itm.stat_changes), itm.name)
+                    self.effect_dict['reverse_effect_player'].append(to_append)
+                    self.effect_dict['reverse_effect_player'].sort()
+                    del to_append
                 else:
-                    self.battle_dict['effect_dict']['active_effect_enemy'] = True
-                    self.battle_dict['effect_dict']['reverse_effect_enemy'].append((self.battle_dict['turn'] + itm.duration, self.reverse_item_stat(itm.stat_changes)))
-                    self.battle_dict['effect_dict']['reverse_effect_enemy'].sort()
+                    to_append = (self.battle_dict['turn'] + itm.duration, self.reverse_item_stat(itm.stat_changes), itm.name)
+                    self.effect_dict['reverse_effect_enemy'].append(to_append)
+                    self.effect_dict['reverse_effect_enemy'].sort()
+                    del to_append
+
+            input('calculated effect queue')
 
         except AttributeError as e:
             debug_info(e, 'An incorrect object type was used as type stat_item in battle_manager.use_item().')
@@ -939,33 +938,35 @@ class battle_manager(ABC):
             return False
 
     def stat_change_writeout(self):
-        if self.battle_dict['effect_dict']['active_effect_player'] is True:
+        if self.effect_dict['reverse_effect_player'] != []:
             print('\nPlayer Status Effects:\n------------------------')
-            i = 1
-            for effect in self.battle_dict['effect_dict']['reverse_effect_player']:
-                print(f"Effect {i}:")
-                print(f"Turns left: {effect[0] - self.battle_dict['turn']}")
-                print(f"Health Modifier: {effect[1][Stat_Sheet.health] * -1}\n" if effect[1][Stat_Sheet.health] != 0 else '', end='')
-                print(f"Strength Modifier: {effect[1][Stat_Sheet.strength] * -1}\n" if effect[1][Stat_Sheet.strength] != 0 else '', end='')
-                print(f"Armor Modifier: {effect[1][Stat_Sheet.armor] * -1}\n" if effect[1][Stat_Sheet.armor] != 0 else '', end='')
-                print(f"Agility Modifier: {effect[1][Stat_Sheet.agility] * -1}\n" if effect[1][Stat_Sheet.agility] != 0 else '', end='')
-                print(f"Power Modifier: {effect[1][Stat_Sheet.power] * -1}" if effect[1][Stat_Sheet.power] != 0 else '', end='')
+            temp_stat_changes = self.effect_dict['reverse_effect_player']
+            for i in range(len(temp_stat_changes)):
+                print(self.effect_dict['reverse_effect_player'][i])
+                print(f"Effect {i + 1}: {temp_stat_changes[i][2]}")
+                print(f"Turns left: {temp_stat_changes[i][0] - self.battle_dict['turn']}")
+                print(f"Health Modifier: {temp_stat_changes[i][1][Stat_Sheet.health] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.health] != 0 else '', end='')
+                print(f"Strength Modifier: {temp_stat_changes[i][1][Stat_Sheet.strength] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.strength] != 0 else '', end='')
+                print(f"Armor Modifier: {temp_stat_changes[i][1][Stat_Sheet.armor] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.armor] != 0 else '', end='')
+                print(f"Agility Modifier: {temp_stat_changes[i][1][Stat_Sheet.agility] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.agility] != 0 else '', end='')
+                print(f"Power Modifier: {temp_stat_changes[i][1][Stat_Sheet.power] * -1}" if temp_stat_changes[i][1][Stat_Sheet.power] != 0 else '', end='')
                 i += 1
+            del temp_stat_changes
 
-
-        if self.battle_dict['effect_dict']['active_effect_enemy'] is True:
+        if self.effect_dict['reverse_effect_enemy'] != []:
             print('\nEnemy Status Effects:\n------------------------')
-            i = 1
-            for effect in self.battle_dict['effect_dict']['reverse_effect_enemy']:
-                print(f"Effect {i}:")
-                print(f"Turns left: {effect[0] - self.battle_dict['turn']}\n")
-                print(f"Health Modifier: {effect[1][Stat_Sheet.health] * -1}\n" if effect[1][Stat_Sheet.health] != 0 else '', end='')
-                print(f"Strength Modifier: {effect[1][Stat_Sheet.strength] * -1}\n" if effect[1][Stat_Sheet.strength] != 0 else '', end='')
-                print(f"Armor Modifier: {effect[1][Stat_Sheet.armor] * -1}\n" if effect[1][Stat_Sheet.armor] != 0 else '', end='')
-                print(f"Agility Modifier: {effect[1][Stat_Sheet.agility] * -1}\n" if effect[1][Stat_Sheet.agility] != 0 else '', end='')
-                print(f"Power Modifier: {effect[1][Stat_Sheet.power] * -1}" if effect[1][Stat_Sheet.power] != 0 else '', end='')
+            temp_stat_changes = self.effect_dict['reverse_effect_player']
+            for i in range(len(temp_stat_changes)):
+                print(temp_stat_changes[i])
+                print(f"Effect {i + 1}:")
+                print(f"Turns left: {temp_stat_changes[i][0] - self.battle_dict['turn']}")
+                print(f"Health Modifier: {temp_stat_changes[i][1][Stat_Sheet.health] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.health] != 0 else '', end='')
+                print(f"Strength Modifier: {temp_stat_changes[i][1][Stat_Sheet.strength] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.strength] != 0 else '', end='')
+                print(f"Armor Modifier: {temp_stat_changes[i][1][Stat_Sheet.armor] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.armor] != 0 else '', end='')
+                print(f"Agility Modifier: {temp_stat_changes[i][1][Stat_Sheet.agility] * -1}\n" if temp_stat_changes[i][1][Stat_Sheet.agility] != 0 else '', end='')
+                print(f"Power Modifier: {temp_stat_changes[i][1][Stat_Sheet.power] * -1}" if temp_stat_changes[i][1][Stat_Sheet.power] != 0 else '', end='')
                 i += 1
-
+            del temp_stat_changes
 
         input('\nPress enter to return to the previous menu.')
 
@@ -1045,6 +1046,7 @@ class battle_manager(ABC):
         pass
 
     def draw_hp(self, plyr, enemy):
+        clr_console()
         prcnt_plyr_health = round(self.percent_health(plyr) / 2)
 
         print(f'{plyr.name}: [', end='')
@@ -1161,7 +1163,15 @@ class battle_manager(ABC):
                     # Convert user_choice to indexable integer
                     user_choice = int(user_choice) - 1
                     # Try to access the selected attack and return it
-                    return plyr.attacks[user_choice]
+                    try:
+                        if plyr.attacks[user_choice].ammo_type in plyr.collection.items is False:
+                            print("You don't have the correct item to use this attack.")
+                        else:
+                            return plyr.attacks[user_choice]
+                    except AttributeError:
+                        return plyr.attacks[user_choice]
+
+
             except (ValueError, IndexError, AttributeError):
                 print('Invalid input.')
 
@@ -1282,7 +1292,7 @@ class battle_manager(ABC):
                 # Determine whose turn it is
                 if self.battle_dict['turn'] == Turn.Attack:
                     def active_debuff_check():
-                        if (self.battle_dict['effect_dict']['active_effect_player'] is True) or (self.battle_dict['effect_dict']['active_effect_enemy'] is True):
+                        if (self.effect_dict['reverse_effect_player'] != []) or (self.effect_dict['reverse_effect_enemy'] != []):
                             return True
                         else:
                             return False
@@ -1290,7 +1300,6 @@ class battle_manager(ABC):
                     while True:
                         try:
                             # Clear console and then redraw HP
-                            clr_console()
                             self.draw_hp(plyr, enemy)
 
                             print("\n1. Attack\n2. Use Item")
@@ -1305,11 +1314,15 @@ class battle_manager(ABC):
                             if user_choice == '1':
                                 # Player chooses to attack #
                                 # Determine attack and use it
+                                self.draw_hp(plyr, enemy)
+
                                 self.switch_turn([temp_power, plyr.stats.power], self.use_attack(plyr, enemy, self.plyr_choose_attack(plyr)))
                             elif user_choice == '2':
                                 # Player choose to use an item #
+                                self.draw_hp(plyr, enemy)
                                 self.switch_turn([temp_power, plyr.stats.power], self.use_item(plyr, self.plyr_choose_item(plyr)))
                             elif (user_choice == '3') and (active_debuff_check() is True):
+                                self.draw_hp(plyr, enemy)
                                 self.stat_change_writeout()
                             else:
                                 input('Invalid input.')
